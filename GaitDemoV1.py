0eb5c7ea24312335a733b8870550ac2bc4a6ce36
xerifg <ethanxrf@163.com>
2021-09-25 15:21:48 +0800

    a demo

--- GaitDemoV1.py
+++ GaitDemoV1.py
@@ -1,76 +1,63 @@
-"""
-Gait demo for Tutorial V1.0
-Author: Chunfeng Song
-E-mail: developfeng@gmail.com
-
-This gait recognition demo needs clean background, e.g. a white wall. As this demo only takes GEI as its feature, so it can only recognize the same view. For example, people walk with the same angle for both registration and recognition.
-"""
-
-# Make sure you have install the following Python packages, we recommend you to install Anaconda and OpencCV 2.4. This code can work in both Windows and Linux.
 import sys
-import cPickle
-from PyQt4 import QtCore, QtGui, uic
-import numpy as n
+from PyQt5 import QtCore, QtWidgets, uic, QtGui
+import numpy as np
 from PIL import Image
 from PIL.ImageQt import ImageQt
 import cv2
 import os
+import pickle
+import realsense_depth as rsd
 
-#Loading the UI window
-qtCreatorFile = "GaitUI.ui" 
+# Loading the UI window
+qtCreatorFile = "GaitUI.ui"
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
-def pickle(filename, data, compress=False):
+
+def pickle2(filename, data, compress=False):
     fo = open(filename, "wb")
-    cPickle.dump(data, fo, protocol=cPickle.HIGHEST_PROTOCOL)
+    pickle.dump(data, fo, protocol=pickle.HIGHEST_PROTOCOL)  # 序列化对象，并将结果数据流写入到文件对象中
     fo.close()
 
-def unpickle(filename):
+
+def unpickle2(filename):
     fo = open(filename, 'rb')
-    dict = cPickle.load(fo)
+    dict = pickle.load(fo)  # 反序列化对象,将文件中的数据解析为一个Python对象
     fo.close()
     return dict
 
-class GaitDemo(QtGui.QMainWindow, Ui_MainWindow):
+
+class GaitDemo(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         '''
         Set initial parameters here.
-        Note that the demo window size is 1366*768, you can edit this via Qtcreator.
+        Note that the demo window size is 1920*1080, you can edit this via Qtcreator.
         In this demo, we take 20 frames of profiles to generate a GEI. You can edit this number by your self.
         '''
-        QtGui.QMainWindow.__init__(self)
+        QtWidgets.QMainWindow.__init__(self)
         Ui_MainWindow.__init__(self)
         self.showFullScreen()
         self.setupUi(self)
-        self.capture = cv2.VideoCapture(0)# Edit this default num to 1 or 2, if you have multiple cameras.
-        self.currentFrame=n.array([])
-        self.firstFrame=None
+        # get camera images
+        self.capture = rsd.DepthCamera()  # through D435 camera
+        # self.capture = cv2.VideoCapture(0)  # through computer cam
+        self.currentFrame = np.array([])
+        self.firstFrame = None
         self.register_state = False
         self.recognition_state = False
         self.save_on = False
         self.gei_fix_num = 20
-        
-        #Ok, this is our logos, welcome to contact and visit us.
-        self.logo1 = QtGui.QLabel(self.centralwidget)
-        self.logo1.setPixmap(QtGui.QPixmap(QtGui.QPixmap('logo1.png').scaled(250,70)))
-        self.logo1.setGeometry(5, 698, 250, 70)
-        self.logo2 = QtGui.QLabel(self.centralwidget)
-        self.logo2.setPixmap(QtGui.QPixmap(QtGui.QPixmap('logo2.png').scaled(170,70)))
-        self.logo2.setGeometry(275, 698, 170, 70)
-        self.logo3 = QtGui.QLabel(self.centralwidget)
-        self.logo3.setPixmap(QtGui.QPixmap(QtGui.QPixmap('logo3.png').scaled(243,70)))
-        self.logo3.setGeometry(480, 698, 243, 70)
-        
-        #Set two window for raw video and segmentation.
-        self.video_lable=QtGui.QLabel(self.centralwidget)
-        self.seg_label = QtGui.QLabel(self.centralwidget)
+
+
+        # Set two window for raw video and segmentation.
+        self.video_lable = QtWidgets.QLabel(self.centralwidget)
+        self.seg_label = QtWidgets.QLabel(self.centralwidget)
         self._timer = QtCore.QTimer(self)
-        self.video_lable.setGeometry(50,100, 512, 384)
-        self.seg_label.setGeometry(612,100, 512, 384)
+        self.video_lable.setGeometry(50, 200, 640, 480)
+        self.seg_label.setGeometry(800, 200, 640, 480)
         self.load_dataset()
         self._timer.timeout.connect(self.play)
-        
-        #Waiting for you to push the button.
+
+        # Waiting for you to push the button.
         self.register_2.clicked.connect(self.register_show)
         self.recognize.clicked.connect(self.recognition_show)
         self.updater.clicked.connect(self.update_bk)
@@ -92,7 +79,7 @@ class GaitDemo(QtGui.QMainWindow, Ui_MainWindow):
         self.register_state = True
         self.recognition_state = False
         self.state_print.setText('Register!')
-        self.gei_current = n.zeros((128,88), n.single)
+        self.gei_current = np.zeros((128, 88), np.single)
         self.numInGEI = 0
 
     def load_dataset(self):
@@ -101,17 +88,17 @@ class GaitDemo(QtGui.QMainWindow, Ui_MainWindow):
         '''
         self.data_path = './GaitData'
         if os.path.exists(self.data_path):
-            dic = unpickle(self.data_path)
+            dic = unpickle2(self.data_path)
             self.num = dic['num']
             self.gei = dic['gei']
             self.name = dic['name']
         else:
             self.num = 0
-            self.gei = n.zeros([100,128,88],n.uint8)
+            self.gei = np.zeros([100, 128, 88], np.uint8)
             self.name = []
-            dic = {'num':self.num, 'gei':self.gei, 'name':self.name}
-            pickle(self.data_path, dic, compress=False)
-        self.id_num.setText('%d' %self.num)
+            dic = {'num': self.num, 'gei': self.gei, 'name': self.name}
+            pickle2(self.data_path, dic, compress=False)
+        self.id_num.setText('%d' % self.num)
         self.state_print.setText('Running!')
 
     def recognition_show(self):
@@ -120,7 +107,7 @@ class GaitDemo(QtGui.QMainWindow, Ui_MainWindow):
         '''
         self.recognition_state = True
         self.register_state = False
-        self.gei_current = n.zeros((128,88), n.single)
+        self.gei_current = np.zeros((128, 88), np.single)
         self.numInGEI = 0
         self.state_print.setText('Recognition!')
 
@@ -134,107 +121,122 @@ class GaitDemo(QtGui.QMainWindow, Ui_MainWindow):
         '''
         Main program.
         '''
-        ret, frame=self.capture.read() #Read video from a camera.
-        if(ret==True):
-            frame = cv2.resize(frame,(512,384))
-            #Apply background subtraction method.
+        # ret, frame = self.capture.read()  # Read video from a camera.
+        ret, depth_frame, frame = self.capture.get_frame()
+        if (ret == True):
+            # frame = cv2.resize(frame, (512, 384))
+            # Apply background subtraction method.
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            gray = cv2.GaussianBlur(gray, (3,3), 0)
+            gray = cv2.GaussianBlur(gray, (3, 3), 0)
             if self.firstFrame is None:
-                self.firstFrame = gray # Set this frame as the background.
+                self.firstFrame = gray  # Set this frame as the background.
             frameDelta = cv2.absdiff(self.firstFrame, gray)
             thresh = cv2.threshold(frameDelta, 50, 255, cv2.THRESH_BINARY)[1]
-            self.FrameForUpdate = gray 
+            self.FrameForUpdate = gray
             thresh = cv2.dilate(thresh, None, iterations=2)
             (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                cv2.CHAIN_APPROX_SIMPLE)
-            thresh = n.array(thresh)
-            max_rec=0
-            
-            #Find the max box.
+                                         cv2.CHAIN_APPROX_SIMPLE)
+            thresh = np.array(thresh)
+            max_rec = 0
+
+            # Find the max box.
             for c in cnts:
                 if cv2.contourArea(c) < 500:
                     continue
                 (x, y, w, h) = cv2.boundingRect(c)
 
-                if w>25 and h>50:
-                    if max_rec<w*h:
-                        max_rec = w*h
+                if w > 25 and h > 50:
+                    if max_rec < w * h:
+                        max_rec = w * h
                         (x_max, y_max, w_max, h_max) = cv2.boundingRect(c)
-            #If exist max box.
-            if max_rec>0:
+            # If exist max box.
+            if max_rec > 0:
                 cv2.rectangle(frame, (x_max, y_max), (x_max + w_max, y_max + h_max), (0, 255, 0), 2)
-                if x_max>20: #To ignore some regions which contain parts of human body.
+                if x_max > 20:  # To ignore some regions which contain parts of human body.
                     if self.register_state or self.recognition_state:
-                        nim = n.zeros([thresh.shape[0]+10,thresh.shape[1]+10],n.single) # Enlarge the box for better result.
-                        nim[y_max+5:(y_max + h_max+5),x_max+5:(x_max + w_max+5)] = thresh[y_max:(y_max + h_max),x_max:(x_max + w_max)]
+                        nim = np.zeros([thresh.shape[0] + 10, thresh.shape[1] + 10],
+                                      np.single)  # Enlarge the box for better result.
+                        nim[y_max + 5:(y_max + h_max + 5), x_max + 5:(x_max + w_max + 5)] = thresh[
+                                                                                            y_max:(y_max + h_max),
+                                                                                            x_max:(x_max + w_max)]
                         offsetX = 20
                         # Get coordinate position.
-                        ty, tx = (nim >100).nonzero()
-                        sy, ey = ty.min(), ty.max()+1
-                        sx, ex = tx.min(), tx.max()+1
+                        ty, tx = (nim > 100).nonzero()
+                        sy, ey = ty.min(), ty.max() + 1
+                        sx, ex = tx.min(), tx.max() + 1
                         h = ey - sy
                         w = ex - sx
-                        if h>w:# Normal human should be like this, the height shoud be greater than wideth.
+                        if h > w:  # Normal human should be like this, the height shoud be greater than wideth.
                             # Calculate the frame for GEI
                             cx = int(tx.mean())
-                            cenX = h/2
-                            start_w = (h-w)/2
-                            if max(cx-sx,ex-cx)<cenX:
-                                start_w = cenX - (cx-sx)
-                            tim = n.zeros((h,h), n.single)
-                            tim[:,start_w:start_w+w] = nim[sy:ey,sx:ex]
-                            rim = Image.fromarray(n.uint8(tim)).resize((128,128), Image.ANTIALIAS)
-                            tim = n.array(rim)[:,offsetX:offsetX+88]
-                            if self.numInGEI<self.gei_fix_num:
-                                self.gei_current += tim # Add up until reaching the fix number.
+                            cenX = h / 2
+                            start_w = (h - w) / 2
+                            if max(cx - sx, ex - cx) < cenX:
+                                start_w = cenX - (cx - sx)
+                            tim = np.zeros((h, h), np.single)
+                            tim[:, start_w:start_w + w] = nim[sy:ey, sx:ex]
+                            rim = Image.fromarray(np.uint8(tim)).resize((128, 128), Image.ANTIALIAS)
+                            tim = np.array(rim)[:, offsetX:offsetX + 88]
+                            if self.numInGEI < self.gei_fix_num:
+                                self.gei_current += tim  # Add up until reaching the fix number.
                             self.numInGEI += 1
-                            
-                        if  self.numInGEI>self.gei_fix_num:
+
+                        if self.numInGEI > self.gei_fix_num:
                             if self.save_on:
-                                #Save the GEI.
-                                self.gei[self.num,:,:] = self.gei_current/self.gei_fix_num
-                                Image.fromarray(n.uint8(self.gei_current/self.gei_fix_num)).save('./gei/gei%02d%s.jpg'%(self.num,self.id_name.toPlainText()))
+                                # Save the GEI.
+                                self.gei[self.num, :, :] = self.gei_current / self.gei_fix_num
+                                Image.fromarray(np.uint8(self.gei_current / self.gei_fix_num)).save(
+                                    './gei/gei%02d%s.jpg' % (self.num, self.id_name.toPlainText()))
                                 self.name.append(self.id_name.toPlainText())
-                                self.num +=1
-                                self.id_num.setText('%d' %self.num)
-                                dic = {'num':self.num, 'gei':self.gei, 'name':self.name}
-                                pickle(self.data_path, dic, compress=False)
+                                self.num += 1
+                                self.id_num.setText('%d' % self.num)
+                                dic = {'num': self.num, 'gei': self.gei, 'name': self.name}
+                                pickle2(self.data_path, dic, compress=False)
                                 self.save_on = False
                                 self.state_print.setText('Saved!')
                             elif self.recognition_state:
-                                #Recognition.
-                                self.gei_query = self.gei_current/(self.gei_fix_num)
-                                score = n.zeros(self.num)
-                                self.gei_to_com = n.zeros([128,88],n.single)
-                                for q in xrange(self.num):
-                                    self.gei_to_com = self.gei[q,:,:]
-                                    score[q]=n.exp(-(((self.gei_query[:]-self.gei_to_com[:])/(128*88))**2).sum())#Compare with gait database.
+                                # Recognition.
+                                self.gei_query = self.gei_current / (self.gei_fix_num)
+                                score = np.zeros(self.num)
+                                self.gei_to_com = np.zeros([128, 88], np.single)
+                                for q in range(self.num):
+                                    self.gei_to_com = self.gei[q, :, :]
+                                    score[q] = np.exp(-(((self.gei_query[:] - self.gei_to_com[:]) / (
+                                                128 * 88)) ** 2).sum())  # Compare with gait database.
                                 q_id = score.argmax()
                                 if True:
-                                    id_rec = '%s' %self.name[q_id]
-                                    cv2.putText(frame,id_rec,(x_max+20,y_max+20),fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1.0, thickness=2,color=(0,0,255))
+                                    id_rec = '%s' % self.name[q_id]
+                                    cv2.putText(frame, id_rec, (x_max + 20, y_max + 20),
+                                                fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1.0, thickness=2,
+                                                color=(0, 0, 255))
             else:
-                self.gei_current = n.zeros((128,88), n.single)
+                self.gei_current = np.zeros((128, 88), np.single)
                 self.numInGEI = 0
-                
-            #Show results.
-            self.currentFrame=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-            self.currentSeg=Image.fromarray(thresh).convert('RGB')
+
+            # Show results.
+            self.currentFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            self.currentSeg = Image.fromarray(thresh).convert('RGB')
             self.currentSeg = ImageQt(self.currentSeg)
-            height,width=self.currentFrame.shape[:2]
-            img=QtGui.QImage(self.currentFrame,
-                              width,
-                              height,
-                              QtGui.QImage.Format_RGB888)
-            img=QtGui.QPixmap.fromImage(img)
+            height, width = self.currentFrame.shape[:2]
+            img = QtGui.QImage(self.currentFrame,
+                                   width,
+                                   height,
+                                   QtGui.QImage.Format_RGB888)
+            img = QtGui.QPixmap.fromImage(img)
             self.video_lable.setPixmap(img)
-            seg=QtGui.QImage(self.currentSeg)
-            seg=QtGui.QPixmap(seg)
+            seg = QtGui.QImage(self.currentSeg)
+            seg = QtGui.QPixmap(seg)
             self.seg_label.setPixmap(seg)
 
+    def keyPressEvent(self, event):  # 重新实现了keyPressEvent()事件处理器。
+        # 按住键盘事件
+        # 这个事件是PyQt自带的自动运行的，当我修改后，其内容也会自动调用
+        if event.key() == QtCore.Qt.Key_Escape:  # 当我们按住键盘是esc按键时
+            self.close()  # 关闭程序
+
+
 if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     window = GaitDemo()
     window.show()
     sys.exit(app.exec_())
--- README.md
+++ README.md
@@ -1,12 +1,4 @@
-------------------------------------------------------------------------
-* Gait demo for Tutorial V1.0                     
-* By Chunfeng Song                                
-* E-mail: chunfeng.song@nlpr.ia.ac.cn                   
-------------------------------------------------------------------------
- ![img](https://github.com/developfeng/GaitRecognition/blob/master/ToyGaitDemo.gif)
-
 i.    Overview
-ii.   Copying
 iii.  Use
 
 i. OVERVIEW
@@ -19,26 +11,19 @@ registration and recognition.
 
 This code is one part of the Tutorial of ICPR 2016.
 
-ii. COPYING
------------------------------
-We share this code only for research use. We neither warrant correctness 
-nor take any responsibility for the consequences of using this code. 
-If you find any problem or inappropriate content in this code, feel 
-free to contact us.
-
-iii. USE
+ii. How to use
 -----------------------------
 This code should work on Windows or Linux, with Python and OpenCV.
-We highly recommend you install Anaconda (python 2.7) and OpenCV2.4:
+We highly recommend you install Anaconda (python 3.8) and OpenCV4.1:
 
-1) You should install the python libraries (including PyQt4, numpy, PIL)and 
+1) You should install the python libraries (including PyQt5, numpy, PIL)and 
 OpenCV.
 2) Type 'python GaitDemoV1.py' in the command line. Then press 'Enter' button to 
 start this demo.
 3) Firstly, you need to type your name in the 'Name' box and click the 'Register'
  button to record the human GEI into the database. Then the human can walk 
- in front of the camera. After several frames, you need to click the 'Save' 
- button to save.
+  in front of the camera. After several frames, you need to click the 'Save' 
+  button to save.
 4) Now, you can click the 'Recognize' button to have a test.
 5) Sometime, you need to click 'UpdataBk' button to refresh the background.
 6) The GEIs will be saved in './gei/'.
